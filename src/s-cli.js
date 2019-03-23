f3bdba2565b57848225f1e52c3917492eadbb8b9
George Aidonidis <george.aidonidis@gmail.com>
2019-03-23 11:09:49 +0200

    feat: Add commander to handle input
    * Expose battery function and stop calling it immediately
    * Use commander to parse user input and show help message
    
    close #8

---
+++ src/s-cli.js
@@ -0,0 +1,16 @@
+const program = require('commander');
+const packageJson = require('../package.json');
+const battery = require('./battery');
+
+program
+	.version(packageJson.version)
+	.option('-b, --battery', 'show battery info')
+	.parse(process.argv);
+
+if (process.argv.slice(2).length === 0) {
+	program.outputHelp();
+}
+
+if (program.battery) {
+	battery();
+}
